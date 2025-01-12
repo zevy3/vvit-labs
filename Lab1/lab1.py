a0eecffd4acb1c5ee30e88b394ca28c151e8ab78
zevy3 <113357159+zevy3@users.noreply.github.com>
2025-01-12 20:58:42 +0300

    Add files via upload

---
+++ Lab1/lab1.py
@@ -0,0 +1,9 @@
+my_list=[1,2,3,4,5,6,7,8,9,10]
+
+print(my_list[0])
+print(my_list[2])
+print(my_list[-1])
+
+my_list[1]=100
+print(my_list[1])
+
---
+++ Lab1/lab1dz.py
@@ -0,0 +1,10 @@
+a=int(input('Введите число: '))
+for i in range(1,a+1):
+    print(i)
+
+b = int(input('Введите первое число: '))
+c = int(input('Введите второе число: '))
+if b>c:
+    print('Большее число: ',b)
+else:
+    print('Большее число: ',c)
